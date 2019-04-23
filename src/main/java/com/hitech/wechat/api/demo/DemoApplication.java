cc74cb14fe1015f0138318bec8f24cf34a3183d9
zhoulin2 <zhoulin2@asiainfo.com>
2019-04-23 09:04:45 +0800

    init

--- src/main/java/com/hitech/wechat/api/demo/DemoApplication.java
+++ src/main/java/com/hitech/wechat/api/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.hitech.wechat.api.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Steven
+ */
 @SpringBootApplication
 public class DemoApplication {
 
